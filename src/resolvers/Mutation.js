60df1d031aeb2446f0eced02fb1932a7d351a626
Sven Le Cann <sven.lecann@live.fr>
2021-02-06 21:53:49 +0100

    Section 4 - 38

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -86,12 +86,17 @@ const Mutation = {
     db.posts.push(post)
 
     if (published) {
-      pubsub.publish('post', { post })
+      pubsub.publish('post', {
+        post: {
+          mutation: 'CREATED',
+          data: post,
+        },
+      })
     }
 
     return post
   },
-  deletePost(parent, args, { db }, info) {
+  deletePost(parent, args, { db, pubsub }, info) {
     const { id } = args
     const postIndex = db.posts.findIndex((post) => post.id === id)
 
@@ -99,16 +104,25 @@ const Mutation = {
       throw new Error('Post not found')
     }
 
-    const deletedPosts = db.posts.splice(postIndex, 1)
+    const [post] = db.posts.splice(postIndex, 1)
 
     db.comments = db.comments.filter((comment) => comment.post !== id)
 
-    return deletedPosts[0]
-  },
-  updatePost(parent, args, { db }, info) {
-    const { id, data } = args
+    if (post.published) {
+      pubsub.publish('post', {
+        post: {
+          mutation: 'DELETED',
+          data: post,
+        },
+      })
+    }
 
+    return post
+  },
+  updatePost(parent, args, { db, pubsub }, info) {
+    const { id, data } = args
     const post = db.posts.find((post) => post.id == id)
+    const originalPost = { ...post }
 
     if (!post) {
       throw new Error('Post not found')
@@ -124,6 +138,29 @@ const Mutation = {
 
     if (typeof data.published === 'boolean') {
       post.published = data.published
+
+      if (originalPost.published && !post.published) {
+        pubsub.publish('post', {
+          post: {
+            mutation: 'DELETED',
+            data: originalPost,
+          },
+        })
+      } else if (!originalPost.published && post.published) {
+        pubsub.publish('post', {
+          post: {
+            mutation: 'CREATED',
+            data: post,
+          },
+        })
+      }
+    } else if (post.published) {
+      pubsub.publish('post', {
+        post: {
+          mutation: 'UPDATED',
+          data: post,
+        },
+      })
     }
 
     return post
